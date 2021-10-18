f9a425321cef6a8fd967cae3577168e3aedbc3f7
droman892 <droman892@gmail.com>
2021-10-18 16:37:46 -0400

    updated

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 // tells express where to serve static files from
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
+  console.log('Welcome to the application');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000);
+
+
+app.listen(PORT, () => console.log(`The application is being served on localhost:${PORT}`));
